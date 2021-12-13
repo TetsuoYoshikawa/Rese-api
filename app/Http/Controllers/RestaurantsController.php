1e898c913a7c5b0aa61ddc06ebf84701bd11a85a
tetsuoYoshikawa <bball.t3y18@gmail.com>
2021-12-13 23:01:16 +0900

    create-rese

--- app/Http/Controllers/RestaurantsController.php
+++ app/Http/Controllers/RestaurantsController.php
@@ -12,7 +12,7 @@ class RestaurantsController extends Controller
 {
     public function get(Request $request)
     {
-        $restaurant = Restaurant::with("prefecture", "genre")->get();
+        $restaurant = Restaurant::with("prefecture", "genre", "favorites")->get();
         $prefecture = Prefecture::get();
         $genre = Genre::get();
         $item = [
